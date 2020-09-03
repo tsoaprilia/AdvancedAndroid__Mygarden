50b9fd27d6abc267cacb406d2fbf045f699264bb
Aprilia Dwi Cristyana <tsoaprilia@gmail.com>
2020-09-03 20:38:59 +0700

    pertama commit kedua commit ketiga commit keempat commit kelima commit keenam commit
    Signed-off-by: Aprilia Dwi Cristyana <tsoaprilia@gmail.com>

--- app/src/main/java/com/example/android/mygarden/ui/PlantListAdapter.java
+++ app/src/main/java/com/example/android/mygarden/ui/PlantListAdapter.java
@@ -18,12 +18,13 @@ package com.example.android.mygarden.ui;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.android.mygarden.R;
 import com.example.android.mygarden.provider.PlantContract.PlantEntry;
