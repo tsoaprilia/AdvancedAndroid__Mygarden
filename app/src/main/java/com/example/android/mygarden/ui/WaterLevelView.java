fde24677bf22e7e9520a7f8b9b6312f992f1e2aa
Aprilia Dwi Cristyana <tsoaprilia@gmail.com>
2020-09-03 20:51:47 +0700

    pertama commit kedua commit ketiga commit keempat commit kelima commit keenam commit ketujuh commit kedelapan commit
    Signed-off-by: Aprilia Dwi Cristyana <tsoaprilia@gmail.com>

--- app/src/main/java/com/example/android/mygarden/ui/WaterLevelView.java
+++ app/src/main/java/com/example/android/mygarden/ui/WaterLevelView.java
@@ -7,9 +7,10 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
-import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
+
+import androidx.core.content.ContextCompat;
 
 import com.example.android.mygarden.R;
 
