6c9f3519cb5850b84838441fd17cdbffd37dec01
Aprilia Dwi Cristyana <tsoaprilia@gmail.com>
2020-09-03 20:33:55 +0700

    pertama commit kedua commit ketiga commit
    Signed-off-by: Aprilia Dwi Cristyana <tsoaprilia@gmail.com>

--- app/src/main/java/com/example/android/mygarden/ui/AddPlantActivity.java
+++ app/src/main/java/com/example/android/mygarden/ui/AddPlantActivity.java
@@ -18,11 +18,12 @@ package com.example.android.mygarden.ui;
 
 import android.content.ContentValues;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.android.mygarden.PlantWateringService;
 import com.example.android.mygarden.R;
