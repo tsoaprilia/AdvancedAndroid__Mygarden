5066720a6b3024b5ba443e3870ef1631bf149468
Aprilia Dwi Cristyana <tsoaprilia@gmail.com>
2020-09-03 20:36:08 +0700

    pertama commit kedua commit ketiga commit keempat commit kelima commit
    Signed-off-by: Aprilia Dwi Cristyana <tsoaprilia@gmail.com>

--- app/src/main/java/com/example/android/mygarden/ui/PlantDetailActivity.java
+++ app/src/main/java/com/example/android/mygarden/ui/PlantDetailActivity.java
@@ -20,13 +20,14 @@ import android.content.ContentUris;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
 import com.example.android.mygarden.PlantWateringService;
 import com.example.android.mygarden.R;
